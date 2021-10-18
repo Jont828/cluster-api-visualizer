5430e59f9508e4b717a666e1099c9b04dfc845f3
Jonathan Tong <jt572@cornell.edu>
2021-10-18 14:49:14 -0400

    Start hacking with tree plugin

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Tree/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tree from './components/Tree.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tree
   }
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/Tree.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container">
+    <vue-tree
+      id="tree"
+      :dataset="treeData"
+      :config="treeConfig"
+    >
+      <template v-slot:node="{ node, collapsed }">
+        <div
+          class="rich-media-node"
+          :style="{ border: collapsed ? '2px solid grey' : '' }"
+        >
+          <!-- <img
+            :src="node.avatar"
+            style="width: 48px; height: 48px; border-raduis: 4px;"
+          /> -->
+          <span>{{ node.name }}</span>
+        </div>
+      </template>
+    </vue-tree>
+  </div>  
+</template>
+
+<script>
+import VueTree from '@ssthouse/vue-tree-chart'
+
+export default {
+  name: 'Tree',
+  components: {
+    VueTree
+  },
+  data() {
+    return {
+      treeData: {
+        name: 'kind-capz',
+        value: 800,
+        children: [
+          {
+            name: 'default-27983',
+            value: 400,
+            children: []
+          },
+          {
+            name: 'Smith',
+            value: 200,
+            children: [
+              {
+                name: 'S1',
+                value: 230,
+              }
+            ]
+          },
+          {
+            name: 'Jackson',
+            value: 300,
+          }
+        ]
+      },
+      treeConfig: { nodeWidth: 250, nodeHeight: 150, levelHeight: 250 }
+      // treeConfig: { nodeWidth: 250, nodeHeight: 150, levelHeight: 250 }
+    }
+  }
+}
+</script>
+
+<style scoped >
+#tree {
+  width: 80%;
+  height: 80%;
+  border: 1px solid black;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+}
+
+.rich-media-node {
+  width: 150px;
+  height: 150px;
+  /* padding: 8px; */
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  color: #2c3e50;
+  background-color: #DAE8FC;
+  border-radius: 4px;
+}
+</style>
