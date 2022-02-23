3fb92af98496e20a64bdcc39e03c17668aaf0945
Jonathan Tong <jt572@cornell.edu>
2022-02-23 17:20:14 -0500

    Refactor to use 404 handler

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"embed"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -10,6 +9,8 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/Jont828/capi-visualization/internal"
 	"k8s.io/client-go/tools/clientcmd"
@@ -20,8 +21,7 @@ import (
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-//go:embed web/dist
-var web embed.FS
+// var web embed.FS
 
 type Client struct {
 	DefaultClient    client.Client
@@ -101,16 +101,89 @@ func main() {
 	http.Handle("/api/v1/custom-resource/", http.HandlerFunc(handleCustomResourceTree))
 	http.Handle("/api/v1/cluster-resources/", http.HandlerFunc(handleClusterResourceTree))
 
-	stripped, err := fs.Sub(web, "web/dist")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// stripped, err := fs.Sub(web, "web/dist")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	frontendFS := http.FileServer(http.FS(stripped))
-	http.Handle("/", frontendFS)
+	// frontendFS := http.FileServer(http.FS(stripped))
+	// http.Handle("/", frontendFS)
+
+	var frontend fs.FS = os.DirFS("web/dist")
+	httpFS := http.FS(frontend)
+	fileServer := http.FileServer(httpFS)
+	serveIndex := serveFileContents("index.html", httpFS)
+
+	http.Handle("/", intercept404(fileServer, serveIndex))
 
 	log.Printf("Listening on port %d\n", port)
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+type hookedResponseWriter struct {
+	http.ResponseWriter
+	got404 bool
+}
+
+func (hrw *hookedResponseWriter) WriteHeader(status int) {
+	if status == http.StatusNotFound {
+		// Don't actually write the 404 header, just set a flag.
+		hrw.got404 = true
+	} else {
+		hrw.ResponseWriter.WriteHeader(status)
+	}
+}
+
+func (hrw *hookedResponseWriter) Write(p []byte) (int, error) {
+	if hrw.got404 {
+		// No-op, but pretend that we wrote len(p) bytes to the writer.
+		return len(p), nil
+	}
+
+	return hrw.ResponseWriter.Write(p)
+}
+
+func intercept404(handler, on404 http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hookedWriter := &hookedResponseWriter{ResponseWriter: w}
+		handler.ServeHTTP(hookedWriter, r)
+
+		if hookedWriter.got404 {
+			on404.ServeHTTP(w, r)
+		}
+	})
+}
+
+func serveFileContents(file string, files http.FileSystem) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Restrict only to instances where the browser is looking for an HTML file
+		if !strings.Contains(r.Header.Get("Accept"), "text/html") {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "404 not found")
+
+			return
+		}
+
+		// Open the file and return its contents using http.ServeContent
+		index, err := files.Open(file)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "%s not found", file)
+
+			return
+		}
+
+		fi, err := index.Stat()
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "%s not found", file)
+
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		http.ServeContent(w, r, fi.Name(), fi.ModTime(), index)
+	}
 }
 
 func handleMultiClusterTree(w http.ResponseWriter, r *http.Request) {
