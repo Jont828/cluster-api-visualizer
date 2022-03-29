3b54789e18291a2ac114dfe78a1c19d6dd7393fa
Jonathan Tong <jt572@cornell.edu>
2022-03-29 19:44:53 -0400

    Rename secret values

--- hack/deploy-to-kind.sh
+++ hack/deploy-to-kind.sh
@@ -7,7 +7,7 @@ fi
 
 KUBECONFIG_DATA=$(kind get kubeconfig --name ${1} --internal)
 
-helm install --generate-name ./helm/capi-visualization --set kubeconfig="$KUBECONFIG_DATA"  || exit 1
+helm install --generate-name ./helm/capi-visualization --set kubeconfig="$KUBECONFIG_DATA" || exit 1
 kubectl rollout status deployment visualize-cluster
 
 echo "Running at http://localhost:8081"
