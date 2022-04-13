179800cf26911b9ac5d27085f119ca87620f93a6
Jonathan Tong <jt572@cornell.edu>
2022-04-13 17:30:16 -0400

    Add Helm docs

---
+++ hack/deploy-repo-to-kind.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <name-of-kind-management-cluster>"
+  echo "Note: existing kind clusters are [ $(kind get clusters) ]"
+  exit 1
+fi
+
+ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
+KUBECONFIG_DATA=$(kind get kubeconfig --name ${1} --internal)
+
+helm install --repo https://raw.githubusercontent.com/Jont828/cluster-api-visualizer/main/helm/repo cluster-api-visualizer --generate-name --set kubeconfig="$KUBECONFIG_DATA" || exit 1
+kubectl rollout status deployment capi-visualizer
+
+echo "Running at http://localhost:8081"
+kubectl port-forward service/capi-visualizer 8081:8081
