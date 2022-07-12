5adaeba9cef6f608eaad6ce7eee0cda4af228add
Jonathan Tong <jt572@cornell.edu>
2022-07-12 17:20:48 -0400

    Update Helm repo

--- hack/update-helm-repo.sh
+++ hack/update-helm-repo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-helm package ${ROOT}/helm/cluster-api-visualizer -d ${ROOT}/helm/repo
-helm repo index ${ROOT}/helm/repo
+
+helm package ${ROOT}/helm/cluster-api-visualizer -d ${ROOT}/helm/repo/new
+helm repo index ${ROOT}/helm/repo/new
+helm repo index --merge ${ROOT}/helm/repo/index.yaml ${ROOT}/helm/repo/new
+mv ${ROOT}/helm/repo/new/index.yaml ${ROOT}/helm/repo/index.yaml
+mv ${ROOT}/helm/repo/new/*.tgz ${ROOT}/helm/repo
