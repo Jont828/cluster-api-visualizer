89408123876be1db784a90d80fd60c7efa936b5f
Jonathan Tong <jt572@cornell.edu>
2022-11-30 17:56:22 -0500

    Self host Roboto and mdi icon fonts

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -2,17 +2,18 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
 import colors from 'vuetify/lib/util/colors'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-theme: {
-  themes: {
-    light: {
-      primary: colors.blue.darken1, 
-      secondary: colors.blue.lighten3, 
-      accent: colors.lightBlue.base,
+  theme: {
+    themes: {
+      light: {
+        primary: colors.blue.darken1,
+        secondary: colors.blue.lighten3,
+        accent: colors.lightBlue.base,
+      },
     },
   },
-},
 });
