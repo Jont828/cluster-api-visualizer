640db0e821ef6b8dc17e30b74903103c8ce23de6
Jonathan Tong <jt572@cornell.edu>
2026-01-12 12:39:23 -0500

    Revert "Fix WebSocket upgrade issue in dev environment"

--- web/vue.config.js
+++ web/vue.config.js
@@ -25,16 +25,5 @@ module.exports = {
         logLevel: 'debug'
       },
     },
-    client: {
-      webSocketTransport: 'sockjs',
-      webSocketURL: 'auto://0.0.0.0:0/ws',
-      overlay: {
-        errors: true,
-        warnings: false,
-      },
-    },
-    webSocketServer: 'sockjs',
-    hot: true,
-    liveReload: true,
   },
 }
